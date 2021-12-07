21bda77f384b85ed2799300af5564552d4617e45
TPOJlb <llirik58764@gmail.com>
2021-12-07 16:26:25 +0200

    Taxi code

--- AddInfoAndPhone.js
+++ AddInfoAndPhone.js
@@ -6,6 +6,7 @@ import {
 } from 'react-native'
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
+
 export default class AddInfo extends React.Component {
     state = {
         username: '', password: '', email: '',phone: ''
--- Screen/Person.js
+++ Screen/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View} from 'react-native'
+import MapView from 'react-native-maps';
 
 export default function App() {
 
@@ -8,10 +8,16 @@ export default function App() {
 
     return (
 
-      <View>
 
-          <Text>23123</Text>
+        <MapView style={{height:'100%',width:'100%'}}
+            initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}
 
-      </View>
+        />
+
     );
 }
