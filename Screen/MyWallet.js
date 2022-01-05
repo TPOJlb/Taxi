fc93a7e2e6820d06dc876eca61e7d783e220af2f
TPOJlb <llirik58764@gmail.com>
2022-01-05 16:32:05 +0200

    Taxi code

--- Screen/MyWallet.js
+++ Screen/MyWallet.js
@@ -1,22 +1,35 @@
-import React, {useEffect, useState } from 'react';
-import {ScrollView,SafeAreaView,Platform,Text,View, StyleSheet,TouchableOpacity,KeyboardAvoidingView,Image } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+    ScrollView,
+    SafeAreaView,
+    Platform,
+    Text,
+    View,
+    StyleSheet,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    Image
+} from 'react-native';
 import {AntDesign, Entypo} from '@expo/vector-icons';
-import { CreditCardInput, LiteCreditCardInput } from "react-native-credit-card-input";
+import {CreditCardInput, LiteCreditCardInput} from "react-native-credit-card-input";
 import {CarB} from "../CarBase";
 import {PayB} from "../PayBase";
 import AddPaymentMethod from "./AddPaymentMethod";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import PayLoad from "../PayLoad";
-import { createStore } from 'redux';
+import {createStore} from 'redux';
 import counter from "../reduxer"
+import * as Location from "expo-location";
 
 
-export default function MyWallet({navigation,route}) {
+export default function MyWallet({navigation, route}) {
 
     const [number, setNumber] = useState('')
     const [expiry, setExpiry] = useState('')
     const [cvc, setCvc] = useState('')
     const [result, setResult] = useState("")
+
+    const [rem, setRem] = useState("")
 
 
     const dsfsd = (form) => {
@@ -27,43 +40,77 @@ export default function MyWallet({navigation,route}) {
     }
 
 
-
     const focus = navigation.isFocused();
 
+    useEffect(() => {
+        (async () => {
+            try {
+                setNumber(await AsyncStorage.getItem('number'))
+                setExpiry(await AsyncStorage.getItem('expiry'))
+                setCvc(await AsyncStorage.getItem('cvc'))
+                setRem(await AsyncStorage.getItem('Money'))
+            } catch (err) {
+                console.log('error signing up: ', err)
+            }
+        })();
+    }, []);
+
     return (
-        <View style={{flex: 1}}>
-            <View style={{flex:1,position: 'absolute',top: 30,left:10,flexDirection:'row'}}>
+        <View style={{flex: 2}}>
 
-                <TouchableOpacity onPress={()=>navigation.openDrawer()}>
+            <View style={{flex: 1, position: 'absolute', top: 30, left: 10, flexDirection: 'row'}}>
 
-                    <Entypo name="menu" size={40} color="black" />
+                <TouchableOpacity onPress={() => navigation.openDrawer()}>
+
+                    <Entypo name="menu" size={40} color="black"/>
                 </TouchableOpacity>
-                <Text>123</Text>
+                <View style={{marginTop:10}}>
+                <Text>   Остаток на счету: {rem} грн</Text>
+                </View>
             </View>
 
-            <View style={{marginTop:70}}>
-                < CreditCardInput placeholders = {{number: number, expiry: expiry, cvc: expiry }} onChange = {dsfsd}/>
-                <TouchableOpacity onPress={async () => {
-                     try {
-                         await AsyncStorage.multiSet([['number', number], ['expiry', expiry], ['cvc', cvc]]);
-                         // const result = await AsyncStorage.multiGet(['number', 'expiry', 'cvc'])
+            <View style={{flex: 4, marginTop: 70}}>
+                < CreditCardInput cardImageFront={require("../Image/gift-card.png")}
+                                  cardImageBack={require("../Image/gift-card.png")}
+                                  labels={{number: "Номер", expiry: "Дата", cvc: "CVC/CCV"}}
+                                  placeholders={{number: number, expiry: expiry, cvc: cvc}} onChange={dsfsd}/>
+                <TouchableOpacity style={{
+                    marginTop: 10,
+                    flex: 1,
+                    backgroundColor: 'white',
+                    marginLeft: 70,
+                    marginRight: 70,
+                    borderRadius: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }} onPress={async () => {
+                    try {
+                        await AsyncStorage.multiSet([['number', number], ['expiry', expiry], ['cvc', cvc]]);
+                        // const result = await AsyncStorage.multiGet(['number', 'expiry', 'cvc'])
                     } catch (err) {
                         console.log('error signing up: ', err)
                     }
                 }}>
-                    <Text>
+                    <View >
+                    <Text >
                         Добавить карту
                     </Text>
+                    </View>
                 </TouchableOpacity>
+
             </View>
-            <ScrollView style={{flex:1}}>
+            {/*<View style={{justifyContent:'center',alignItems:'center'}}>*/}
+            {/*<Text >Список всех операций</Text>*/}
+            {/*</View>*/}
+            <ScrollView style={{weight:'40%',height:'20%'}}>
                 {
+
                     PayB.map(item => {
-                        return(
-                            <View style={{alignItems: 'center',justifyContent: 'center'}}>
-                                <Text>{item.date} = {item.price} $ </Text>
-                            </View>
-                        );
+                            return (
+                                <View style={{alignItems: 'center', justifyContent: 'center',backgroundColor:'black',marginLeft:85,marginRight:85,marginTop:10,padding:10,borderRadius:20}}>
+                                    <Text style={{color:'white'}}>{item.date} = {item.price} $ </Text>
+                                </View>
+                            );
 
                         }
                     )
@@ -71,24 +118,23 @@ export default function MyWallet({navigation,route}) {
             </ScrollView>
 
 
-            <ScrollView style={{flex: 1}}>
+            <ScrollView style={{flex:1}}>
 
                 <View style={{alignItems: 'center', justifyContent: 'center'}}>
-                    <TouchableOpacity onPress={async () => {
+                    <TouchableOpacity  onPress={async () => {
                         try {
                             setResult(await AsyncStorage.multiGet(['number', 'expiry', 'cvc']))
-                            console.log(result)
-                            const resul = await AsyncStorage.multiGet(['number', 'expiry', 'cvc'])
-                            console.log(resul)
                         } catch (err) {
                             console.log('error signing up: ', err)
                         }
                     }}>
-
-                        <Text>ggbljhg</Text>
+                        <View style={{marginTop:5,backgroundColor:'green',borderRadius:5}}>
+                        <Text style={{color:'white'}}>Просмотреть добавленные карты</Text>
+                        </View>
                     </TouchableOpacity>
-                    <Text>  </Text>
-                    {(result !== '')? <Text>{result[0][1]} {result[1][1]} {result[2][1]}</Text>:null}
+                    {(result !== '') ? <View><Text> Номер: {result[0][1]}</Text>
+                        <Text> Дата: {result[1][1]}    CVC/CCV: {result[2][1]}</Text>
+                        </View>: null}
 
                 </View>
             </ScrollView>
@@ -96,7 +142,6 @@ export default function MyWallet({navigation,route}) {
 
         </View>
     );
-
 
 
 }
